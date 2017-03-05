b18e6c6244c73fe9587f80b3ba6fad38a829c5f5
boogity <harumph505@gmail.com>
2017-03-04 19:32:21 -0600

    piglatin program needs more work

---
+++ piglatin.py
@@ -0,0 +1,11 @@
+pyg = 'ay'
+original = raw_input('Enter a word:')
+word = original.lower()
+first = word[0]
+new_word = word[1:len(word)] + first + pyg
+
+if len(original) > 0 and original.isalpha():
+    print "Your word was" + original
+    print "\n It shall be magically transformed into the piglatin word " + new_word + "! AMAZING!"
+else:
+    print 'empty'
